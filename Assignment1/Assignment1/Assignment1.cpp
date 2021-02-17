bd7e004476bcec10c071c500793461db858b3a9f
Rick Droogh <rickdroogh@hotmail.com>
2021-02-17 16:47:38 +0100

    Changed to while loop over alle images untill the rms is not smaller then previous

--- Assignment1/Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1/Assignment1.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
     FileStorage fs(inputSettingsFile, FileStorage::READ);
     if (!fs.isOpened())
     {
-        cout << "Could not open the configuration file: \"" << inputSettingsFile << "\"" << endl;
+        std::cout << "Could not open the configuration file: \"" << inputSettingsFile << "\"" << endl;
         parser.printMessage();
         return -1;
     }
@@ -33,72 +33,29 @@ int main(int argc, char** argv)
     Size image_size;
 
     Size board_size = Size(s.boardSize);
-    vector<vector<Point2f>> image_points;
-    vector<vector<Point3f>> object_points;
-    vector<Point3f> objects;
+    Mat overall_dist_coeffs;
+    Mat overall_intrinsic = Mat(3, 3, CV_32FC1);
 
-    for (int i = 0; i < board_size.height; i++)
-        for (int j = 0; j < board_size.width; j++)
-            objects.push_back(Point3f((float)j * s.squareSize, (float)i * s.squareSize, 0));
+    overall_intrinsic.ptr<float>(0)[0] = 1;
+    overall_intrinsic.ptr<float>(1)[1] = 1;
 
-    if (s.inputType == 1)
+    bool done = false;
+    double previous_rms = 10;
+    while(done == false)
     {
-        image_amount = 0;
-        clock_t image_taken_time = clock();
-        VideoCapture camera = s.inputCapture;
-        if (!camera.isOpened()) {
-            cerr << "ERROR: Could not open camera" << endl;
-            return 1;
-        }
+        vector<vector<Point2f>> image_points;
+        vector<vector<Point3f>> object_points;
+        vector<Point3f> objects;
 
-        namedWindow("Webcam", WINDOW_AUTOSIZE);
-        while (image_amount < 5)
+        for (int i = 0; i < board_size.height; i++)
+            for (int j = 0; j < board_size.width; j++)
+                objects.push_back(Point3f((float)j * s.squareSize, (float)i * s.squareSize, 0));
+
+        vector<String> images = s.imageList;
+        for (int image_amount = 0; image_amount < images.size(); image_amount++)
         {
-            Mat gray_image;
-            vector<Point2f> image_corners;
-
-            camera >> image;
+            image = imread(images[image_amount], IMREAD_COLOR);
             image_size = image.size();
-            imshow("Webcam", image);
-            cvtColor(image, gray_image, COLOR_BGR2GRAY);
-            //bool found_chessboard = false;
-            bool found_chessboard = findChessboardCorners(gray_image, board_size, image_corners);
-
-            if (found_chessboard)
-            {
-                cornerSubPix(gray_image, image_corners, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 30, 0.1));
-                drawChessboardCorners(image, board_size, Mat(image_corners), found_chessboard);
-
-                if (waitKey(10) == 32)
-                {
-                    image_points.push_back(image_corners);
-                    object_points.push_back(objects);
-
-                    image_amount += 1;
-                    image_taken_time = clock();
-
-                    std::cout << "printing" << image_amount;
-                    namedWindow("Photo " + to_string(image_amount), WINDOW_AUTOSIZE);
-                    imshow("Photo " + to_string(image_amount), image);
-                }
-            }
-            else {
-                if (waitKey(10) == 27)
-                {
-                    break;
-                }
-            }
-        }
-
-        camera.release();
-    }
-    else
-    {
-        image = s.nextImage();
-        image_size = image.size();
-        image_amount = 0;
-        while (!image.empty() and image_amount < s.nrFrames)
-        {
             Mat gray_image;
             vector<Point2f> image_corners;
             cvtColor(image, gray_image, COLOR_BGR2GRAY);
@@ -112,11 +69,9 @@ int main(int argc, char** argv)
                 image_points.push_back(image_corners);
                 object_points.push_back(objects);
             }
-            //namedWindow("Image display");
-            //imshow("window", image);
+            namedWindow("Image display");
+            imshow("window", image);
             //waitKey(0);
-
-            image_amount += 1;
             image = s.nextImage();
 
             if (waitKey(10) == 27)
@@ -124,21 +79,119 @@ int main(int argc, char** argv)
                 break;
             }
         }
+
+        Mat intrinsic = overall_intrinsic;
+        Mat dist_coeffs = overall_dist_coeffs;
+        vector<Mat> rvecs;
+        vector<Mat> tvecs;
+
+        double rms = calibrateCamera(object_points, image_points, image_size, intrinsic, dist_coeffs, rvecs, tvecs);
+        if (rms < previous_rms)
+        {
+            overall_intrinsic = intrinsic;
+            overall_dist_coeffs = dist_coeffs;
+            previous_rms = rms;
+        }
+        else 
+        {
+            done = true;
+        }
+
+        std::cout << "\n Root mean square: " << rms;
+        std::cout << "\n intrinsic: " << intrinsic;
+        std::cout << "\n overall_intrinsic: " << overall_intrinsic;
+
+        std::cout << "\n distCoeffs: " << dist_coeffs << "\n";
+        std::cout << "\n overall_dist_coeffs: " << overall_dist_coeffs << "\n";
     }
-
-    Mat intrinsic = Mat(3, 3, CV_32FC1);
-    Mat distCoeffs;
-    vector<Mat> rvecs;
-    vector<Mat> tvecs;
-
-    intrinsic.ptr<float>(0)[0] = 1;
-    intrinsic.ptr<float>(1)[1] = 1;
-
-    double rms = calibrateCamera(object_points, image_points, image_size, intrinsic, distCoeffs, rvecs, tvecs);
-
-    cout << "\n Root mean square: " << rms;
-    cout << "\n intrinsic: " << intrinsic;
-    cout << "\n distCoeffs: " << distCoeffs << "\n";
 
     return 0;
 }
+
+
+//if (false)
+//{
+//    if (s.inputType == 1)
+//    {
+//        image_amount = 0;
+//        clock_t image_taken_time = clock();
+//        VideoCapture camera = s.inputCapture;
+//        if (!camera.isOpened()) {
+//            cerr << "ERROR: Could not open camera" << endl;
+//            return 1;
+//        }
+//        namedWindow("Webcam", WINDOW_AUTOSIZE);
+//        while (image_amount < 5)
+//        {
+//            Mat gray_image;
+//            vector<Point2f> image_corners;
+
+//            camera >> image;
+//            image_size = image.size();
+//            imshow("Webcam", image);
+//            cvtColor(image, gray_image, COLOR_BGR2GRAY);
+//            //bool found_chessboard = false;
+//            bool found_chessboard = findChessboardCorners(gray_image, board_size, image_corners);
+
+//            if (found_chessboard)
+//            {
+//                cornerSubPix(gray_image, image_corners, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 30, 0.1));
+//                drawChessboardCorners(image, board_size, Mat(image_corners), found_chessboard);
+
+//                if (waitKey(10) == 32)
+//                {
+//                    image_points.push_back(image_corners);
+//                    object_points.push_back(objects);
+
+//                    image_amount += 1;
+//                    image_taken_time = clock();
+
+//                    std::cout << "printing" << image_amount;
+//                    namedWindow("Photo " + to_string(image_amount), WINDOW_AUTOSIZE);
+//                    imshow("Photo " + to_string(image_amount), image);
+//                }
+//            }
+//            else {
+//                if (waitKey(10) == 27)
+//                {
+//                    break;
+//                }
+//            }
+//        }
+
+//        camera.release();
+//    }
+//    else
+//    {
+//        image = s.nextImage();
+//        image_size = image.size();
+//        image_amount = 0;
+//        while (!image.empty() and image_amount < s.nrFrames)
+//        {
+//            Mat gray_image;
+//            vector<Point2f> image_corners;
+//            cvtColor(image, gray_image, COLOR_BGR2GRAY);
+//            bool found_chessboard = findChessboardCorners(gray_image, board_size, image_corners);
+
+//            if (found_chessboard)
+//            {
+//                cornerSubPix(gray_image, image_corners, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 30, 0.1));
+//                drawChessboardCorners(image, board_size, Mat(image_corners), found_chessboard);
+
+//                image_points.push_back(image_corners);
+//                object_points.push_back(objects);
+//            }
+//            namedWindow("Image display");
+//            imshow("window", image);
+//            //waitKey(0);
+
+//            image_amount += 1;
+//            image = s.nextImage();
+
+//            if (waitKey(10) == 27)
+//            {
+//                break;
+//            }
+//        }
+//    }
+//}
